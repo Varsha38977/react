f07dfa8b968c5b07dfbd68f51b7f780f42db2c87
VARSHA <varsha.lalwani.che16@itbhu.ac.in>
2020-03-20 18:35:00 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React ,{ Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  constructor() {
+    super();
+    this.state=
+    {
+      color: "red",
+      doctors:
+      [
+        {
+          "name":"Dr. Rohit",
+          "rating" : 46
+        }
+        ,
+        {
+          "name":"Dr. shobhit",
+          "rating" : 66
+        }
+        ,
+        {
+          "name":"Dr. Sunad",
+          "rating" : 76
+        }
+      ]
+    };
+  }
+  render () {
+    return(
+        <div>
+            <ol>
+                {
+                    this.state.doctors.map((item, key) => {
+                      if(item.rating<=50){
+                        return <li key={key}>{item.name} <font color="red">{item.rating}</font></li>
+                      }
+                      if(item.rating>50 && item.rating<=70){
+                        return <li key={key}>{item.name} <font color="yellow">{item.rating}</font></li>
+                      }
+                      if(item.rating>70 && item.rating<=100){
+                        return <li key={key}>{item.name} <font color="green">{item.rating}</font></li>
+                      }
+                    })
+                }
+            </ol>
+        </div>
+    )
+}
+
 }
 
 export default App;
